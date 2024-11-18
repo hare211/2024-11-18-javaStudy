42a91f870ba783656b0f2039fa39b737a8260f0e
sist-117 <sist-117@HelloWorld>
2024-11-18 14:34:03 +0900

    1111

--- FirstProject/src/첫번째자바.java
+++ FirstProject/src/첫번째자바.java
@@ -72,6 +72,7 @@
  */
 
 
+
 public class 첫번째자바 {
 
 	public static void main(String[] args) {
---
+++ secondProject/src/secondProject/두번째.java
@@ -0,0 +1,11 @@
+package secondProject;
+
+public class 두번째 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("두번째 Hello");
+
+	}
+
+}
