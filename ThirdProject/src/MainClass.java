1422b3d703dcaba759fb35bdf4b3a957949c51dc
sist-117 <sist-117@HelloWorld>
2024-11-18 15:34:35 +0900

    212

--- ThirdProject/src/MainClass.java
+++ ThirdProject/src/MainClass.java
@@ -1,4 +1,20 @@
-
+/*
+ 	자바 실행
+ 	========
+ 	** 면접
+ 	컴파일
+ 		=> 파일 => 기계언어로 변경 (컴퓨터가 인식하는 언어)
+ 		   Java / C# / C / C++
+ 	인터프리터 : 한 줄씩 읽어서 출력
+ 				HTML / Java...
+ 				
+	A.java ====== A.class ====== 화면에 출력
+		    javac		java 
+		    	0101000101
+    public class MainClass
+    {
+    }
+ */
 public class MainClass {
 
 	public static void main(String[] args) {
