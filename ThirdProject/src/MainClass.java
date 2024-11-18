b4bd73ea5bf6a1edd6b9e7bdafab3016a40a7b39
sist-117 <sist-117@HelloWorld>
2024-11-18 16:51:38 +0900

    24/11/18

--- ThirdProject/src/MainClass.java
+++ ThirdProject/src/MainClass.java
@@ -1,4 +1,25 @@
 /*
+ 	1. 자바 설치
+ 	path :	c:\jdk-17.0.2\bin == > java/javac
+ 	classpath : 자바에서 지원하는 라이브러리를 사용
+ 				.;c:\jdk-17.0.2\lib
+ 				== 현재 풀더
+	2. 이클립스
+		1) File => new => JavaProject
+		2) src 안에 자바 파일 생성
+		
+		변수 / 연산자 / 제어문 / 배열
+		public class ClassName{
+			프로그램의 시작
+			public static void main(String[] args) {
+				//시작 => 코딩
+			}
+		}
+	3. GIT
+		=> team ==> shareProject
+		=> team ==> commit
+ 	
+ 	
  	자바 실행
  	========
  	** 면접
