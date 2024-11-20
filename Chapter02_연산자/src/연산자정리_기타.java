f3b06dc698e27a6a32320d16f644f4ba72525bf6
sist-117 <sist-117@HelloWorld>
2024-11-20 17:49:00 +0900

    24/11/20

---
+++ Chapter02_연산자/src/연산자정리_기타.java
@@ -0,0 +1,20 @@
+/*
+ * 	특수문자
+ * 	\n	==> new line : 다음 줄에 출력
+ * 	\t	==> tab : 일정 간격 띄움
+ * 	\"	==> " 를 출력
+ * 	\\	==> \ 를 출력 => 경로명 
+*/
+public class 연산자정리_기타 {
+	public static void main(String[] args) {
+		System.out.println("c:\\javaDev\\javaStudy");
+		System.out.println("Hello \n");
+		System.out.println("Java!!");
+		System.out.println("\n");
+		System.out.println();
+		System.out.println("Hello\tHello\tHello");
+		System.out.println("\"홍길동\""); // "홍길동"
+		
+	}
+
+}
