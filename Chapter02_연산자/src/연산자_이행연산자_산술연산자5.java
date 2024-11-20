868d5abfceea1d58257951c17ee05477b84ecacf
sist-117 <sist-117@HelloWorld>
2024-11-20 12:51:30 +0900

    24/11/20

---
+++ Chapter02_연산자/src/연산자_이행연산자_산술연산자5.java
@@ -0,0 +1,24 @@
+// 5 ~ 8장
+/*
+ *		+, -, *, /, %
+ *		--		------
+*/
+public class 연산자_이행연산자_산술연산자5 {
+
+	public static void main(String[] args) {
+		// %
+		
+		int a = 369;
+		
+		int f = a / 100;
+		int s = (a % 100) / 10;
+		int t = a % 10;
+		
+		System.out.println(f);
+		System.out.println(s);
+		System.out.println(t);
+		
+		
+	}
+
+}
