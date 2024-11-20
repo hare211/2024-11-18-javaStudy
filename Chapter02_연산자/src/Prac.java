96a8be54d06c195c43a50ea2b0591a5317f6c64d
sist-117 <sist-117@HelloWorld>
2024-11-20 16:30:18 +0900

    24/11/20

---
+++ Chapter02_연산자/src/Prac.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Prac {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		int x = scanner.nextInt();
+		int y = scanner.nextInt();
+		
+		System.out.println(x % y);
+	}
+}
