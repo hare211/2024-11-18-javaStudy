ae804a287995f8459b9e03ab8f380724dde28c20
sist-117 <sist-117@HelloWorld>
2024-11-28 10:22:10 +0900

    24/11/28

---
+++ Chapter03_문자열/src/문자열_1.java
@@ -0,0 +1,55 @@
+/*
+ * 	String 클래스 => char[]을 제어
+ * 	=> 문자 여러 개 저장하는 공간
+ * 		-------- 문자열 : "" char => ''
+ * 	=> 문자열을 저장하는 일반 데이터형식 / 기능(메소드)을 가지는 클래스형
+ * 	=> 웹 / 윈도우 => 자바로 만들 수 있는 모든 프로그램의 핵심
+ * 		=> 오라클 : varchar2 / clob, Mysql : varchar, text
+ * 		=> C / C++ : 문자열이 없다 => char[], char*
+ * 	=> 문자열 저장하는 방법
+ * 		String name = "홍길동"
+ * 		------ ----
+ * 		 데이터형 변수명
+ * 	=> 클래스형으로 저장하는 방법
+ * 		String name = new String("홍길동");
+ * 	=> String 에서 지원하는 기능(메소드)
+ * 		# 메소드
+ * 			리턴형(처리 결과값) 메소드명(매개변수...) {
+ * 			--------------------------------
+ * 			데이터형			|기능		|어떤 값을 첨부
+ * 				요청처리
+ * 				결과값 전송 return;
+ * 			}
+ * 			리턴형 : 결과값 1 개
+ * 			매개변수 : 없는 경우 / 한 개인 경우 / 여러 개
+ * 					 random()	substring() ---- substring(1, 4)
+ * 					 length()
+ * 			메소드 : 라이브러리 => 이미 만들어져 있는 상태
+ * 					*** 사용자 정의 => 직접 만들어서 처리
+ * 						-------- 조립
+ * 			*** 객체지향 프로그램 : 변수 / 메소드
+ * 			------------------------------
+ * 		String 에서 사용하는 메소드
+ * 		1) length() : 문자의 길이 (한글 / 알파벳) => 동일
+ * 						* 알파벳 => 저장 시 1 byte : ASC / Unicode
+ * 						* 한글 => 저장 시 2 byte : Unicode
+ * 							=> "UTF-8"
+ * 			예)
+ * 				String word = "홍길동"
+ * 				int len = word.length(); word 안에 있는 글자의 개수
+ * 				=> len = 3
+ * 				=> 비밀번호 글자수, 입력여부 확인
+ * 					if (word.length() < 1) => 입력이 안된 상태
+ * 				=> 유효성 검사
+ * 		2) charAt => 원하는 위치의 문자 한 개 추출
+*/
+public class 문자열_1 {
+	public static void main(String[] args) {
+		String msg = "Hello Java";
+		int len = msg.indexOf('J');
+		System.out.println(len);
+		len = msg.indexOf("Java");
+		System.out.println(len);
+	}
+
+}
