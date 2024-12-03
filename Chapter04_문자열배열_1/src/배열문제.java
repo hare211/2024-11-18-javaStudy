b6fc8e954c781ae33aad4459267b163ed7d70ddc
hare211 <blackhole2748@gmail.com>
2024-12-03 17:49:19 +0900

    24/12/03

--- Chapter04_문자열배열_1/src/배열문제.java
+++ Chapter04_문자열배열_1/src/배열문제.java
@@ -1,5 +1,7 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
+import java.util.List;
 
 public class 배열문제 {
 	public static void main(String[] args) {
@@ -85,7 +87,7 @@ public class 배열문제 {
 		*/
 		
 		// 6 번
-		
+		/*
         int[] arr = new int[10];
         for (int i = 0; i < 100; i++) {
             int num = (int)(Math.random() * 10);
@@ -96,7 +98,7 @@ public class 배열문제 {
         for (int i = 0; i < arr.length; i++) {
 			System.out.println(i + "의 개수 : " + arr[i]);
 		}
-		
+		*/
 		
 		// 7 번
 		/*
@@ -107,7 +109,27 @@ public class 배열문제 {
 		}
 		Arrays.sort(arr);
 		System.out.println(Arrays.toString(arr));
+		
+		System.out.println(arr.toString());
+		
+		char[] arrReverse = new char[5];
+		int j = 0;
+		for (int i = arr.length - 1; i >= 0; i--) {
+			arrReverse[j] = arr[i];
+			j++;
+		}
+		System.out.println(Arrays.toString(arrReverse));
+		arr = arrReverse;
+		System.out.println(Arrays.toString(arr));
+		
+		System.out.println(arr.toString());
+		System.out.println(arrReverse.toString());
 		*/
+	
+		
+		// AES 정렬 / DESC 정렬
+		
+		
 		
 		
 		
