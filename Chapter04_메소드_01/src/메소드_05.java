50698633015cdc57b66e9fac4cf425be58529fb1
hare211 <blackhole2748@gmail.com>
2024-12-04 12:39:17 +0900

    24/12/04

--- Chapter04_메소드_01/src/메소드_05.java
+++ Chapter04_메소드_01/src/메소드_05.java
@@ -6,6 +6,50 @@
  * 	3. 출력
  * 
 */
+
+import java.util.Scanner;
+
 public class 메소드_05 {
+	static int userInput(int a) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print(a + "' 정수 입력 : ");
+		int c = scanner.nextInt();
+		return c;
+	}
+	
+	static int total(int a, int b, int c) {
+		return a + b + c;
+	}
+	
+	static double div(int total) {
+		return total / 3.0;
+	}
+	static void print (int total, double div) {
+		System.out.println("총점 : " + total);
+		System.out.println("평균 : " + div);
+	}
+	public static void main(String[] args) {
+		/*Scanner scanner = new Scanner(System.in);
+		System.out.print("1' 정수 입력 : ");
+		int a = scanner.nextInt();
+		
+		System.out.print("2' 정수 입력 : ");
+		int b = scanner.nextInt();
+		
+		System.out.print("3' 정수 입력 : ");
+		int c = scanner.nextInt();
+		
+		int total = a + b + c;
+		double avg = total / 3.0;
+		
+		System.out.println("총점 : " + total + " 평균 : " + avg);
+		*/
+		
+		userInput(3);
+		userInput(3);
+		userInput(3);
+		
+		int total = total(0, 0, 0);
+	}
 
 }
---
+++ Chapter04_메소드_01/src/메소드_6.java
@@ -0,0 +1,24 @@
+/*
+ * 	메소드 호출 => 매개변수
+ * 	1) Call By Value
+ * 		=> 값만 전송
+ * 	2) Call By Reference
+ * 		=> 주소값 전송
+*/
+public class 메소드_6 {
+	static void swap(int a, int b) {
+		System.out.println("swap 메소드 진입");
+		System.out.println("사용자가 요청한 값");
+		System.out.println("a = " + a);
+		System.out.println("b = " + b);
+		System.out.println("swap 메소드 요청 완료");
+	}
+	// 시작 = 종료 = 자동 호출
+	// 사용자 정의는 자동 호출이 안된다
+	public static void main(String[] args) {
+		System.out.println("main 시작");
+		swap(10, 20);
+		System.out.println("main 종료 => 프로그램 종료");
+	}
+
+}
