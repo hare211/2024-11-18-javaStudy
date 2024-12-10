b88cd6820dc018ed6230f25c4763d662f821d2f4
hare211 <blackhole2748@gmail.com>
2024-12-10 17:51:10 +0900

    24/12/10

---
+++ Chapter05_객체지향프로그램_3/src/클래스_문제.java
@@ -0,0 +1,23 @@
+class Student {
+	String name;
+	int ban;
+	int no;
+	int kor;
+	int eng;
+	int math;
+}
+class Member {
+	String name, id, password;
+	int age;
+	
+	public Member(String name, String id) {
+		super();
+		this.name = name;
+		this.id = id;
+	}
+	
+	
+}
+public class 클래스_문제 {
+
+}
