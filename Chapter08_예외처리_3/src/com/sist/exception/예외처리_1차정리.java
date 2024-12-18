32858d4f52b6aededd91059d863ed1400b585211
hare211 <blackhole2748@gmail.com>
2024-12-18 17:55:07 +0900

    24/12/18

--- Chapter08_예외처리_3/src/com/sist/exception/예외처리_1차정리.java
+++ Chapter08_예외처리_3/src/com/sist/exception/예외처리_1차정리.java
@@ -55,7 +55,7 @@ public class 예외처리_1차정리 {
 		
 		예외처리_1차정리 c = new 예외처리_1차정리();
 		c.display();
-		
+		 
 		for (int i = 0; i < 10; i++) { 
 			double a = (Math.random() * 10) + 1;
 			int b = (int) (Math.random() * 3); 
