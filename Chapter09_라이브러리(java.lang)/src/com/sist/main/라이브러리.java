268e20e1909d4ff0e1dafd752ab892d3c5f62f8a
hare211 <blackhole2748@gmail.com>
2024-12-19 11:22:33 +0900

    24/12/19

---
+++ Chapter09_라이브러리(java.lang)/src/com/sist/main/라이브러리.java
@@ -0,0 +1,86 @@
+package com.sist.main;
+/*
+ * 자바에서 지원하는 라이브러리
+ * 				-------- 프로그램에 필요한 클래스를 모아서 개발 시 사용
+ * => 라이브러리 중심 => 조립
+ * => Scanner / System / String...
+ *  라이브러리
+ *  	1) 자바에서 지원
+ *  	-	java.lang
+ *  	=>	Object
+ *  		System
+ *  		String
+ *  		StringBuffer
+ *  		Math
+ *  		Wrapper
+ *  		------
+ *  		Integer
+ *  		Double
+ *  		Boolean
+ *  		Byte
+ *  		Float
+ *  		Long
+ *  
+ *  	-	java.util
+ *  	=>	Scanner
+ *  		Date / Calendar
+ *  		Collection
+ *  		-----------> 배열 대체
+ *  		List / Set / Map
+ *  		----------------- interface	  |Queue
+ *  		List : ArrayList, Vector, LinkedList, Stack
+ *  		Set : TreeSet / HashSet : 중복 없는 데이터
+ *  		Map : HashMap / HashTable
+ *  	-	java.io
+ *  	=>	FileReader / FileWriter
+ *  		InputStream / OutputStream
+ *  		BufferedReader / BufferedWriter
+ *  		BufferedInputStream / BufferedOutputStream
+ *  		File
+ *  		~Stream, Reader / Writer
+ *  		-------	 ---------------
+ *  		 1byte		  2byte(한글포함)
+ *  		  |네트워크, 파일업-다운로드
+ *  		입출력 : 파일 / 메모리 / 네트워크
+ *  	-	java.sql
+ *  	=>	Connection
+ *  		Statement
+ *  		ResultSet
+ *  	-	java.net
+ *  	=>	URL / ServerSocket / Socket
+ *  	-	java.text
+ *  	=>	SimpleDateFormat / MessageFormat / DecimalFormat
+ *  	-	java.reflect
+ *  	=>	리플렉션 => 메소드 / 클래스 / 변수 / 생성자
+ *  	****Annotation, enum
+ *  	J2Se
+ *  	웹 라이브러리 : JSP / Servelt / Spring => 2차 자바
+ *  	J2EE
+ *  	J2ME(X) => Kotlin => Dart(Flutter)
+ *  
+ *  
+ *  		=> javax.http
+ *  		=> savax.servlet.http
+ *  		=> org.spingframework
+ *  
+ *  	Java / Oracle / JSP / Spring / Spring-Boot
+ *  	------------------------------------------ Back
+ *  	HTML / CSS / JavaScript / TypeScript
+ *  	-----------------------
+ *  					|
+ *  					JQuery / VueJS / ReactJS / NodeJS / NextJS
+ *  					-------------------------------------------
+ *  	1. 데이터 분석 : 파이썬(넘파이, 판다스, Matplotlib) 
+ *  					Spring <=> 파이썬 - Django
+ *  	2. 검색 엔진 : ElasticSearch : CRUD / 검색
+ *  
+ *  	2) 외부업체 지원 /JSoup lombok / Spring
+ *  		=> my.repository.com...
+ *  		=> simple-json / bson
+ *  	3) 사용자 정의 : 잘 만들어진 클래스 / 자주 쓰이는 클래스
+ *  		=> .jar
+ *  		=>
+ */
+public class 라이브러리 {
+
+}
