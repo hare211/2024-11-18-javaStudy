4ba1ee71a1399d589cc9cd86805d4bcaa1041e77
hare211 <blackhole2748@gmail.com>
2024-12-20 16:01:01 +0900

    24/12/20

--- Chapter10_라이브러리(java.util)/src/com/sist/util/라이브러리_2.java
+++ Chapter10_라이브러리(java.util)/src/com/sist/util/라이브러리_2.java
@@ -1,6 +1,11 @@
 package com.sist.util;
 // StringTokenizer
 /*
+ * givDev 풀더
+ *  - 이클립스 paste 
+ *  - projectStudy 
+ */
+/*
  * 1. 자른 데이터를 읽기 => nextToken() // 몇 개를 잘랐는 지 알 때
  * 2. 총 자른 개수 : counterTokens()
  * 3. 자른 데이터가 있는 동안(Loop) : hasMoreTokens() // 몇 개를 잘랐는 지 모를 때
@@ -9,5 +14,5 @@ package com.sist.util;
  * 5. 네트워크 입출력
  */
 public class 라이브러리_2 {
-
+	Object obj;
 }
