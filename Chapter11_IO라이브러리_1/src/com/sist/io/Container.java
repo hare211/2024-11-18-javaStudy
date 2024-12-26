f2528a5991d80f483a5821f87b7c5bcb1919a238
hare211 <blackhole2748@gmail.com>
2024-12-26 09:53:15 +0900

    주석 두 줄 추가

--- Chapter11_IO라이브러리_1/src/com/sist/io/Container.java
+++ Chapter11_IO라이브러리_1/src/com/sist/io/Container.java
@@ -4,7 +4,8 @@ import com.sist.model.*;
 import java.util.*;
 
 public class Container {
-	// 클래스를 모아서 관리
+	// 클래스를 모아서 관리 => 관련된 클래스가 여러 개 있는 경우
+	// Model(브라우저 전송), DAO => 스프링
 	private Map<String, I> clsMap = new HashMap<String, I>();
 	
 	public Container() {
