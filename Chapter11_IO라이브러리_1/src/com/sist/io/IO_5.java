1c56011409329985f98f977db73ca986b57f17a9
hare211 <blackhole2748@gmail.com>
2024-12-26 10:23:46 +0900

    File - 새로운 파일 생성하기

--- Chapter11_IO라이브러리_1/src/com/sist/io/IO_5.java
+++ Chapter11_IO라이브러리_1/src/com/sist/io/IO_5.java
@@ -3,7 +3,17 @@ import java.io.*;
 // 풀더 / 파일 / (풀더 / 파일 존재 여부 확인) => 삭제(자료실에서 업로더가 삭제 등)
 public class IO_5 {
 	public static void main(String[] args) {
-		File dir = new File("c:\\java_data"); // java_data 풀더 생성하기
+		File file = new File("c:\\java_data\\sawon"); // sawon 파일 생성하기
+		
+		if (!file.exists()) {
+			try {
+				file.createNewFile(); // 파일 생성
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		} else {
+			System.out.println("이미 파일이 존재합니다.");
+		}
 //		if (!dir.exists()) { // 풀더 생성 => 존재하지 않는 경우
 //			dir.mkdir();
 //		} else {
