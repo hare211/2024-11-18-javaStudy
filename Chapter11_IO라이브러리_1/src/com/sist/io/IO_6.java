fc274891f99404384e8ea39a72e22b17e5b80fcc
hare211 <blackhole2748@gmail.com>
2024-12-26 10:33:56 +0900

    File - 주석 정리

--- Chapter11_IO라이브러리_1/src/com/sist/io/IO_6.java
+++ Chapter11_IO라이브러리_1/src/com/sist/io/IO_6.java
@@ -1,7 +1,27 @@
 package com.sist.io;
 // 삭제 => delete()
 import java.io.*;
-
+/*
+ * 1. 파일 읽기
+ * 		new File("경로명\\파일명")
+ * 2. 풀더 읽기
+ * 		new File("경로명\\풀더명")
+ * 
+ * 		웹에서 계속 필요 => 이미지 올리기 / 업로드 / 다운로드
+ * 						--------------------------
+ * 						=> 자료실
+ * 						=> 댓글 - 이미지(이모티콘)
+ * 3. 주요 메서드
+ * 		getName() : 파일명
+ * 		getPath() : 경로명 ~ 파일명
+ * 		length() : 파일 크기
+ * 
+ * 		listFiles() : 풀더의 파일 목록
+ * 		mkdir() : 풀더 생성
+ * 		createNewFile() : 파일 생성
+ * 		delete() : 삭제
+ * 		exits() : 존재 여부 확인
+ */
 public class IO_6 {
 	public static void main(String[] args) {
 		File dir = new File("c:\\java_data");
