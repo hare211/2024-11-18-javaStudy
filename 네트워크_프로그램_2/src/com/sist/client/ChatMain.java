7c1b1c3450137e36bd2a01ea2c3c7e68f6b67e5b
hare211 <blackhole2748@gmail.com>
2024-12-30 14:18:14 +0900

    로그인 창

---
+++ 네트워크_프로그램_2/src/com/sist/client/ChatMain.java
@@ -0,0 +1,26 @@
+package com.sist.client;
+import java.util.*;
+import java.net.*;
+import java.awt.CardLayout;
+import java.awt.HeadlessException;
+import java.io.*;
+import javax.swing.*;
+import com.sist.commons.*;
+
+public class ChatMain extends JFrame {
+	Login login = new Login();
+	CardLayout card = new CardLayout(); // 화면을 숨긴 뒤 보여준다
+	// <jsp:include>
+	public ChatMain() {
+		setLayout(card);
+		add("LOG", login);
+		
+		setSize(800, 600);
+		setVisible(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+	public static void main(String[] args) {
+		new ChatMain();
+	}
+}
