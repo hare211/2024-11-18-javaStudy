adeef9611d5ffa133bf91e89ba71c2d25ec70d0a
hare211 <blackhole2748@gmail.com>
2025-01-03 11:03:15 +0900

    미완성

--- 네트워크_프로그램_2/src/com/sist/server/Server.java
+++ 네트워크_프로그램_2/src/com/sist/server/Server.java
@@ -112,6 +112,7 @@ public class Server implements Runnable{
 				// 클라이언트로부터 값을 수신
 			} catch (Exception ex) {
 				ex.printStackTrace();
+				closeStreams();
 			}
 			/*
 			 * 자바 프로그램에서 가장 쉬운 프로그램
@@ -121,85 +122,30 @@ public class Server implements Runnable{
 			 */
 		}
 		// 실제 통신
+
 		public void run() {
 			try {
 				while (true) {
+					if (in == null) {
+						return;
+					}
 					// 1. 사용자 요청값 받기
 					String msg = in.readLine();
+					if (msg == null) {
+						break;
+					}
 					System.out.println("Client => " + msg);
 					// 로그인 => 100|id|name|sex -> 전송
-					StringTokenizer st = new StringTokenizer(msg, "|");
-					int protocol = Integer.parseInt(st.nextToken()); // 100
+					handleProtocol(msg);
 					
-					switch (protocol) {
-					case Function.LOGIN: { // 로그인 요청 시
-						// 로그인 시 회원가입 내용을 전송(회원가입 미구현(DB X) -> StringTokenizer 로 대체)
-						id = st.nextToken();
-						name = st.nextToken();
-						sex = st.nextToken();
-						pos = "대기실";
-						
-						// 1. 접속이 된 사람 => 정보 전송
-						messageAll(Function.LOGIN + "|"
-								+ id + "|" + name + "|" + sex + "|" + pos);
-						// 2. 입장 메세지 전송
-						messageAll(Function.WAITCHAT + "|[알림]" + name + "님 입장하셨습니다");
-						// 3. waitVc 에 저장
-						waitVc.add(this);
-						// 5. 화면을 변경
-						messageTo(Function.MYLOG + "|" + id);
-						// 6. 접속된 모든 정보를 로그인 된 사람에게 전송
-						for (Client c : waitVc) {
-							messageTo(Function.LOGIN + "|" + c.id + "|" + c.name + "|" + c.sex + "|" + c.pos);
-						}
-					}
-					break;
-					case Function.WAITCHAT:{
-						messageAll(Function.WAITCHAT + "|[" + name + "] " + st.nextToken());
-					}
-					break;
-					case Function.EXIT:{
-						messageAll(Function.EXIT + "|" + id);
-						messageAll(Function.WAITCHAT + "|[알림]" + name + "님이 퇴장하셨습니다.");
-						messageTo(Function.MYEXIT + "|");
-						// 행위를 하는 사람 => this
-						for (int i = 0; i < waitVc.size(); i++) { // 나가기 시 제거
-							Client c = waitVc.get(i);
-							if (c.id.equals(id)) {
-								waitVc.remove(i);
-								try {
-									in.close();
-									out.close();
-								} catch (Exception ex) {
-									ex.printStackTrace();
-								} break;
-							}
-						}
-					}
-					break;
-					/*
-					 * 서버의 기능
-					 * 1. 저장
-					 * 2. 수정
-					 * 3. 읽기 / 쓰기(송수신)
-					 * 4. 검색
-					 */
-					case Function.INFO: {
-						String yid = st.nextToken();
-						for (Client c : waitVc) {
-							if (yid.equals(c.id)) {
-								messageTo(Function.INFO + "|" + c.id + "|" + c.name + "|" + c.sex + "|" + c.pos);
-								break;
-							}
-						}
-					}
-					break;
-					}
 				}
+					
 			} catch (Exception ex) {
 				ex.printStackTrace();
 			}
+		
 		}
+
 		// 접속자 전체 : 대기실
 		public synchronized void messageAll(String msg) {
 			for (int i = 0; i < waitVc.size(); i++) {
@@ -208,6 +154,7 @@ public class Server implements Runnable{
 					c.messageTo(msg); // 모두에게 전송
 				} catch (Exception ex) {
 					waitVc.remove(i); // 여러 번 연결 시도에도 오류 발생 시, 명단에서 삭제
+					c.closeStreams();
 					ex.printStackTrace();
 				}
 			}
@@ -216,6 +163,108 @@ public class Server implements Runnable{
 		public synchronized void messageTo(String msg) {
 			try {
 				out.write((msg + "\n").getBytes()); // 메세지 보내기
+			} catch (Exception ex) {
+				ex.printStackTrace();
+				try {
+					if (out != null) {
+						out.close();
+						out = null;
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		private void closeStreams() {
+			try {
+				if (in != null) {
+					in.close();
+					in = null;
+				}
+				if (out != null) {
+					out.close();
+					out = null;
+				}
+				if (s != null) {
+					s.close();
+					s = null;
+				}
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
+		}
+		private void handleProtocol(String msg) {
+			
+			try {
+			StringTokenizer st = new StringTokenizer(msg, "|");
+			int protocol = Integer.parseInt(st.nextToken()); // 100
+			
+			switch (protocol) {
+			case Function.LOGIN: { // 로그인 요청 시
+				// 로그인 시 회원가입 내용을 전송(회원가입 미구현(DB X) -> StringTokenizer 로 대체)
+				id = st.nextToken();
+				name = st.nextToken();
+				sex = st.nextToken();
+				pos = "대기실";
+				
+				// 1. 접속이 된 사람 => 정보 전송
+				messageAll(Function.LOGIN + "|"
+						+ id + "|" + name + "|" + sex + "|" + pos);
+				// 2. 입장 메세지 전송
+				messageAll(Function.WAITCHAT + "|[알림]" + name + "님 입장하셨습니다");
+				// 3. waitVc 에 저장
+				waitVc.add(this);
+				// 5. 화면을 변경
+				messageTo(Function.MYLOG + "|" + id);
+				// 6. 접속된 모든 정보를 로그인 된 사람에게 전송
+				for (Client c : waitVc) {
+					messageTo(Function.LOGIN + "|" + c.id + "|" + c.name + "|" + c.sex + "|" + c.pos);
+				}
+			}
+			break;
+			case Function.WAITCHAT:{
+				messageAll(Function.WAITCHAT + "|[" + name + "] " + st.nextToken());
+			}
+			break;
+			case Function.EXIT:{
+				messageAll(Function.EXIT + "|" + id);
+				messageAll(Function.WAITCHAT + "|[알림]" + name + "님이 퇴장하셨습니다.");
+				messageTo(Function.MYEXIT + "|");
+				// 행위를 하는 사람 => this
+				for (int i = 0; i < waitVc.size(); i++) { // 나가기 시 제거
+					Client c = waitVc.get(i);
+					if (c.id.equals(id)) {
+						waitVc.remove(i);
+						try {
+							in.close();
+							out.close();
+						} catch (Exception ex) {
+							ex.printStackTrace();
+							closeStreams();
+						} break;
+					}
+				}
+			}
+			break;
+			/*
+			 * 서버의 기능
+			 * 1. 저장
+			 * 2. 수정
+			 * 3. 읽기 / 쓰기(송수신)
+			 * 4. 검색
+			 */
+			case Function.INFO: {
+				String yid = st.nextToken();
+				for (Client c : waitVc) {
+					if (yid.equals(c.id)) {
+						messageTo(Function.INFO + "|" + c.id + "|" + c.name + "|" + c.sex + "|" + c.pos);
+						break;
+					}
+				}
+			}
+			break;
+			
+		}
 			} catch (Exception ex) {
 				ex.printStackTrace();
 			}
