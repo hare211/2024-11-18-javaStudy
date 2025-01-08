eddb6c781665267b60eea5c35fde24174bd34fc9
hare211 <blackhole2748@gmail.com>
2025-01-08 11:49:25 +0900

    VO 데이터 저장 클래스 작성

---
+++ 오라클데이터베이스_3/src/com/sist/dao/EmpDAO.java
@@ -0,0 +1,13 @@
+package com.sist.dao;
+// DAO : 데이터베이스와 연결하는 클래스
+import java.util.*;
+import java.sql.*;
+import com.sist.vo.*;
+/*
+ 	오라클 => SQL
+ 			---- 자바에서도 실행 여부를 확인
+	
+ */
+public class EmpDAO {
+
+}
---
+++ 오라클데이터베이스_3/src/com/sist/vo/EmpVO.java
@@ -0,0 +1,101 @@
+package com.sist.vo;
+// 사원 정보를 모아서 관리 => 사원 한 명에 대한 정보를 저장
+// => 모아서 한 번에 윈도우나 브라우저로 전송하는 목적
+/*
+ *  EMPNO        NUMBER(4) = int
+ 	ENAME        VARCHAR2(10) = String
+ 	JOB          VARCHAR2(9)
+ 	MGR          NUMBER(4)
+ 	HIREDATE     DATE = java.util.Date
+ 	SAL          NUMBER(7,2)
+ 	COMM         NUMBER(7,2)
+ 	DEPTNO       NUMBER(2)
+ 	
+ 	=> 오라클
+ 	문자
+ 		= CHAR / VARCHAR2 / CLOB => String
+	숫자
+		= NUMBER => int / double
+	날짜
+		= DATE => java.util.Date
+ */
+// DAO 오라클 연결 역할
+// VO 데이터 저장 역할
+import java.util.*;
+// 테이블 연동 : 다른 테이블의 컬럼을 추가하면 안된다 / 한 테이블 당 한 클래스를 할당하여 데이터를 저장
+/*
+ 	테이블 여러 개의 컬럼을 조인하여 사용한다면
+ 	--------------------------------
+ 	| 한 개의 테이블로 변경 => View (가상 테이블)
+ */
+public class EmpVO {
+	private int empno, mgr, sal, comm, deptno;
+	private String ename, job;
+	private Date hiredate;
+	
+	// 조인 => 포함 클래스
+	private DeptVO dvo = new DeptVO();
+	private SalGradeVO svo = new SalGradeVO();
+	
+	public DeptVO getDvo() {
+		return dvo;
+	}
+	public void setDvo(DeptVO dvo) {
+		this.dvo = dvo;
+	}
+	public SalGradeVO getSvo() {
+		return svo;
+	}
+	public void setSvo(SalGradeVO svo) {
+		this.svo = svo;
+	}
+	public int getEmpno() {
+		return empno;
+	}
+	public void setEmpno(int empno) {
+		this.empno = empno;
+	}
+	public int getMgr() {
+		return mgr;
+	}
+	public void setMgr(int mgr) {
+		this.mgr = mgr;
+	}
+	public int getSal() {
+		return sal;
+	}
+	public void setSal(int sal) {
+		this.sal = sal;
+	}
+	public int getComm() {
+		return comm;
+	}
+	public void setComm(int comm) {
+		this.comm = comm;
+	}
+	public int getDeptno() {
+		return deptno;
+	}
+	public void setDeptno(int deptno) {
+		this.deptno = deptno;
+	}
+	public String getEname() {
+		return ename;
+	}
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+	public String getJob() {
+		return job;
+	}
+	public void setJob(String job) {
+		this.job = job;
+	}
+	public Date getHiredate() {
+		return hiredate;
+	}
+	public void setHiredate(Date hiredate) {
+		this.hiredate = hiredate;
+	}
+
+}
