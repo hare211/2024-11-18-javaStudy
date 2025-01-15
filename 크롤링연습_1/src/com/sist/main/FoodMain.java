1545bc94f8e82bfcc95f39aae60c06c6bbfac5dc
hare211 <blackhole2748@gmail.com>
2025-01-15 15:39:56 +0900

    menupan 크롤링

--- 크롤링연습_1/src/com/sist/main/FoodMain.java
+++ 크롤링연습_1/src/com/sist/main/FoodMain.java
@@ -33,6 +33,55 @@ public class FoodMain {
 					// 식당 address
 					Element address = doc2.selectFirst("div.areaBasic dl.restAdd dd.add1");
 					System.out.println(address.text());
+					// 식당 score
+					Element score = doc2.selectFirst("div.areaBasic dl.restGrade span.total");
+					System.out.println(score.text());
+					// 식당 theme
+					Element theme = doc2.selectFirst("div.areaBasic dl.restTheme dd.Theme");
+					System.out.println(theme.text());
+					// 식당 price
+					Element price = doc2.selectFirst("div.restPrice p.price");
+					System.out.println(price.text());
+					
+					// poster, time, parking, content
+					// 식당 time
+					Element time = doc2.selectFirst("div.tabInfo ul.tableTopA dd.txt2");
+					System.out.println(time.text());
+					// 식당 content
+					Element content = doc2.selectFirst("div.tabInfo ul.tableBottom div#info_ps_f"); // id 이기 때문에 #
+					System.out.println(content.text()); // <div id="info_ps_f">
+					// 식당 parking
+					Elements tableLR = doc2.select("div.tabInfo ul.tableLR dt");
+					Element parking = null;
+					for (int k = 0; k < tableLR.size(); k++) {
+						String s = tableLR.get(k).text();
+						if (s.equals("주차")) {
+							parking = doc2.select("div.tabInfo ul.tableLR dd").get(k);
+							System.out.println(parking.text());
+						}
+					}
+					// 식당 poster
+					Element poster = doc2.selectFirst("div.areaThumbnail img#rest_bigimg");
+					System.out.println(poster.attr("src"));
+					// 식당 images
+					Elements image = doc2.select("div#id_restphoto_slides img[src*=/restimg/]");
+					String images = "";
+					for (int k = 0; k < image.size(); k++) {
+						images += image.get(k).attr("src") + ",";
+					}
+					images = images.substring(0, images.lastIndexOf(","));
+					//Elements images = doc2.select("ul#id_restphoto_list_ul img");
+					System.out.println(images);
+					/*
+					 <dl class="restGrade"> *** 최상위클래스
+					<dt>평점</dt>
+					<dd class="rate">
+					<p class="point"><span class="star" style="width:67.8571428571429%"></span><!-- ☆☆☆☆☆ //--></p>
+					<p class="score"><span class="total">3.4</span><span class="line">|</span><span class="count">7명 참여</span></p>
+					</dd>
+					restGrade 최상위 클래스 다음 바로 원하는 클래스
+					.text() 태그 무시하고 문자열만 가져온다
+					 */
 				}
 				System.out.println("=================");
 			}
